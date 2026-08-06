3113a7bcfa49616b6a4efbc004e9de8bfb166f8e
smc18418-pixel <smc18418@gmail.com>
2026-08-06 19:09:36 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+// ربط Supabase
+const SUPABASE_URL = "https://mltfdadqekleuileimreva.supabase.co";
+const SUPABASE_KEY = "sb_publishable_0G-4Pauntplhqs9spQmoPg_TPrx5Kn6";
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
