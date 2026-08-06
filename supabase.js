302cff526336b01559c249ee15027c4261c869e8
smc18418-pixel <smc18418@gmail.com>
2026-08-06 21:50:53 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-// ربط Supabase
-const SUPABASE_URL = "https://mltfdadqekleuileimreva.supabase.co";
-const SUPABASE_KEY = "sb_publishable_0G-4Pauntplhqs9spQmoPg_TPrx5Kn6";
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
