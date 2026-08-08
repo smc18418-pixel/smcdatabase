dec18e0028e3c0221db3b34d556a08564391e2bd
smc18418-pixel <smc18418@gmail.com>
2026-08-08 16:42:21 +0300

    Update certgen.js

--- js/certgen.js
+++ js/certgen.js
@@ -15,7 +15,7 @@ const COORDS = {
   register: {
     image: "assets/cert_register_template.jpg",
     name:       { x: 1654, y: 1650, maxW: 4600, size: 200, align: "center" },
-    rank:       { x: 1830, y: 1850, maxW: 1400,  size: 44, align: "right"  },
+    rank:       { x: 1830, y: 1850, maxW: 1400,  size: 88 , align: "right"  },
     date:       { x: 230,  y: 1850, maxW: 400,  size: 40, align: "right"  },
     code:       { x: 1900, y: 1935, maxW: 620,  size: 44, align: "right"  },
     duration:   { x: 1400, y: 2090, maxW: 700,  size: 42, align: "right"  },
