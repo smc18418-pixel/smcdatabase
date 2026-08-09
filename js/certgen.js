1e80a834582c0550224c98f5d01fcf9758b501b2
smc18418-pixel <smc18418@gmail.com>
2026-08-09 14:03:02 +0200

    Update certgen.js

--- js/certgen.js
+++ js/certgen.js
@@ -74,7 +74,7 @@ function fmtDateArabic(d) {
   const monthName = months[dt.getMonth()]; // اسم الشهر
   const year = dt.getFullYear(); // السنة
 
-  return `${day} / ${monthName} / ${year}`;
+  return `${monthName} / ${day} / ${year}`;
 }
 
 async function generateRegistrationCertificate(member) {
